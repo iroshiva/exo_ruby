a8f3d47d388549b6f33ffa8015df7b91dc9c9379
iroshiva <jean.weil@protonmail.com>
2019-07-08 18:55:51 +0200

    exo9

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Salut, quel ton prénom?"
+user_name = gets.chomp
+
+puts "Quel est ton nom de famille?"
+user_surname = gets.chomp
+
+puts "Salut, #{user_name} #{user_surname}!" # toujours séparer les variables !!!!!
