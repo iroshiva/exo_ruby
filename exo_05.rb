a36a20e09bbf7ab9d6e8123ef96521de6b0522fb
iroshiva <jean.weil@protonmail.com>
2019-07-08 18:02:32 +0200

    exo5 terminé

--- exo_05.rb
+++ exo_05.rb
@@ -1,22 +1,24 @@
-puts "On va compter le nombre d'heures de travail à THP"
-puts "Travail : #{10 * 5 * 11}"
-puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+puts "On va compter le nombre d'heures de travail à THP" # affiche le message
+puts "Travail : #{10 * 5 * 11}" # #{} affiche le résultat de ce qu'elle contient, ici nombres d'heure
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"  # affiche le nombre de minutes
 
-puts "Et en secondes ?"
+puts "Et en secondes ?" # affiche le message
 
-puts 10 * 5 * 11 * 60 * 60
+puts 10 * 5 * 11 * 60 * 60 # affiche le résultat de la multiplication
 
-puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # affiche la question
 
-puts 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7 # affichage du résultat de l'opération; réponse par true or false = booleens !!!
 
-puts "Ça fait combien 3 + 2 ? #{3 + 2}"
-puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # affiche la reponse a la question
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # affiche la reponse a la question
 
-puts "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !" # affiche le message
 
-puts "C'est drôle ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :" # affiche le message
 
-puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
-puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
-puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # reponse par true or false, ici true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # false
+
+# #{    }
