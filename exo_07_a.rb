17c9305cb20aee6b4b9dab934d8879ac5ebdd719
iroshiva <jean.weil@protonmail.com>
2019-07-08 18:25:18 +0200

    exo7a  finit

---
+++ exo_07_a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quoi ton blase ?" 
+user_name = gets.chomp  # gets = créer un champ d'écriture pour que l'utilisateur puise ecrire son prenom, qui est stocké dans la variable user_name
+puts user_name # affiche la variable user_name
