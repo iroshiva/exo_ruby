4b39a1668f3f05c5337aa4222bd4d24773a8f404
iroshiva <jean.weil@protonmail.com>
2019-07-08 19:13:58 +0200

    exo_10 finit!

---
+++ exo_10.rb
@@ -0,0 +1,9 @@
+puts "Quelle est ton année de naissance?"
+year_birth = gets.chomp.to_i  # to_i = to integer pour fixer le nombre 
+annee_actuelle = 2017
+
+puts " Tu as eu #{annee_actuelle - year_birth} en #{annee_actuelle}!"
+
+# gets = get string
+# puts = put string
+# si je ne veux pas de string : p ou g
