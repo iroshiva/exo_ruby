b014d72ed24d7ba7da3318b576e5863b5d4bb7c1
iroshiva <jean.weil@protonmail.com>
2019-07-08 18:10:52 +0200

    exo6 finit

--- exo_06.rb
+++ exo_06.rb
@@ -1,5 +1,5 @@
-number_of_hours_worked_per_day = 10
+number_of_hours_worked_per_day = 10 # valeur 10 dans variable number_of_hours_worked_per_day  
 number_of_days_worked_per_week = 5
 number_of_weeks_in_THP = 11
 
-puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}" # résultat du calcul
