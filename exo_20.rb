363ddf1de51c931f40523897f6e9901ccdffa202
iroshiva <jean.weil@protonmail.com>
2019-07-09 13:24:41 +0200

    ajout exo20

---
+++ exo_20.rb
@@ -0,0 +1,13 @@
+# Construis un programme exo_20.rb qui va demander à l'utilisateur un nombre entre 1 et 25 et qui va sortir une pyramide à descendre d' autant d'étages que ce nombre. Voici un exemple :
+
+# Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?
+# > 5
+# Voici la pyramide :
+# #
+# ##
+# ###
+# ####
+# #####
+
+
+puts "Balance un nombre entre 1 et 25!"
