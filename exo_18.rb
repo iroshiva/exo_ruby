c03b926725f89d5af6ea8aa0e02187ea8a8df2b0
iroshiva <jean.weil@protonmail.com>
2019-07-09 12:17:01 +0200

    exo18 finit

--- exo_18.rb
+++ exo_18.rb
@@ -1,6 +1,22 @@
-Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
 
-"jean.dupont.01@email.fr"
-"jean.dupont.02@email.fr"
-etc..
 
+nbr_emails = 50
+i = 0 # COMPTEUR
+emails = []
+
+# boucle jusqu'à 50
+while i <= nbr_emails 
+	i += 1
+
+	# condition : si mon compteur est inférieur à 10 boucles, que le deuxième chiffre qui compte 
+	if i < 10
+		emails << "jean.dupond.0" + i.to_s + "@email.fr"
+
+	# sinon, donc à partir de 10, comptage normal
+	else 
+		emails << "jean.dupond." + i.to_s + "@email.fr"
+	end
+end
+
+# affiche l'array
+p emails
