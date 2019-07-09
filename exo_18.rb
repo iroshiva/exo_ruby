4df0d7a3627cc5b0c1ab600fdee884cca0abe797
iroshiva <jean.weil@protonmail.com>
2019-07-09 11:22:15 +0200

    ajout intitulé exo18

---
+++ exo_18.rb
@@ -0,0 +1,6 @@
+Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
+
+"jean.dupont.01@email.fr"
+"jean.dupont.02@email.fr"
+etc..
+
