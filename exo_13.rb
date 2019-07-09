326b3843b1ca7f3c9fd21ffb9e1a4b8030751cb2
iroshiva <jean.weil@protonmail.com>
2019-07-09 09:53:54 +0200

    fin exo13

--- exo_13.rb
+++ exo_13.rb
@@ -1,8 +1,8 @@
 puts "Quel est votre année de naissance?"
 annee_naiss = gets.chomp.to_i
-i = 2018
+ann_actuel = 2018
 
-while annee_naiss <= i # while : permet de compter !!
+while annee_naiss <= ann_actuel # while : permet de compter !!
 	puts annee_naiss
 	annee_naiss +=1
 end
