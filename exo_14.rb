6b254ea8925f84ca540ce127d6d07944216e070c
iroshiva <jean.weil@protonmail.com>
2019-07-09 09:54:37 +0200

    fin exo15

--- exo_14.rb
+++ exo_14.rb
@@ -2,9 +2,9 @@ puts "Balance un nombre!"
 print "> "
 num = gets.chomp.to_i
 
-i = 0
+fin_compte_a_rebour = 0
 
-while num >= i
+while num >= fin_compte_a_rebour
 	puts num
 	num -= 1
 end
--- exo_15.rb
+++ exo_15.rb
@@ -1,18 +1,10 @@
 puts "Quelle est votre année de naissance?"
 ann_naiss = gets.chomp.to_i
-age = 0
-ann_donne = 2017
+ann_donne = 2018
+i = ann_naiss
 
-
-while ann_naiss <= ann_donne
-	puts ann_naiss
-	ann_naiss += 1
+while i <= ann_donne
+	age = i - ann_naiss
+	puts " #{i} #{age} " # antislash = dit que caractère suivant ets spécial : ex n = a la ligne
+	i += 1
 end
-
-while ann_naiss <= ann_donne
-	puts age
-	age += 1
-end
-
-# idée : age = ann_donne - ann_naiss
-# voir si on peut un décompte inversé... fonction reverse ????
